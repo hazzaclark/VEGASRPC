f95afad8c29564501b19e758fdd3187831d5cedd
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-05-29 01:10:22 +0100

    updated enum to delegate flag types

--- VEGAS4Discord/Config/DisplayDetailType.cs
+++ VEGAS4Discord/Config/DisplayDetailType.cs
@@ -1,7 +1,14 @@
-﻿namespace VegasDiscordRPC {
-    public enum DisplayDetailType {
-        TRACKS,
-        PROJECT_FILENAME,
-        MEDIA_EVENTS
+using System;
+
+namespace VegasDiscordRPC 
+{
+    [Flags]
+    public enum DisplayDetailType
+    {
+        TRACKS = 1,
+        PROJECT_FILENAME = 2,
+        MEDIA_EVENTS = 4,
+
+        TRACK_AND_FILENAME = TRACKS | PROJECT_FILENAME
     }
 }
